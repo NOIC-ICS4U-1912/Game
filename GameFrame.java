6325e97515bd60072f97891ed432fb8421592525
Dave Zhao <36692409+Dave1007@users.noreply.github.com>
2020-02-27 13:53:05 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,48 +1,85 @@
-package game;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+package TheProject;
 
+import java.awt.*;
 import javax.swing.*;
-
+import java.util.ArrayList;
 /**
- * Write a description of class GameFrame here.
+ * Write a description of class Position here.
  *
- * Leslie
- * 2.24
+ * @author (your name)
+ * @version (a version number or a date)
  */
-public class GameFrame extends JFrame implements KeyListener {
-    public static void main (String [] args) {
-        new GameFrame();
+
+public class Charactor extends Thread
+{
+    private Position pos;
+    private ImageIcon img;
+    private int speed;
+    private boolean isAlive;
+    private String dir;
+    private String collide;
+    private boolean isOnGround;
+    private boolean isJumpDie;
+    
+    public void Charactor(){
+        this.pos = new Position();
+        this.pos.setPos(1,1);
+        this.isAlive = true;
+        this.dir = "right";
+        this.isOnGround = true;
+        this.collide = "no";
+        this.isJumpDie = false;
+        this.isAlive = true;
     }
     
-    public GameFrame() {
-        this.setTitle("The Hardest Game in This Planet! ");
-        this.setSize(1920,1080);
-                
-        this.addKeyListener(this);
+    public Position getPos(){
+        return this.pos;
+    }
+    
+    public String getDir(){
+        return this.dir;
+    }
+   
+    public void setDir(String newDir){
+        this.dir = newDir;
+    }
+    
+    public void jump() throws InterruptedException{
+        for(int i = 0 ; i < 150; i++){
+            int xs = this.speed;
+            int ys = -i*i + 150*i;
+            if(this.collide.equals(this.dir)){
+                xs = 0;
+            }
+            if(this.collide.equals("up")){
+                i = 150 - i;
+                ys = -i*i + 150*i;
+            }
+            this.pos.setPos(this.pos.getPosX()+xs, this.pos.getPosY() + ys);
+            Thread.sleep(10);
+            if(this.isOnGround){
+                break;
+            }
+        }
+    }
+    
+    public void run(){
+        while(true){
+            if(this.collide.equals(this.dir)){
+                this.speed = 0;
+            }else{
+                this.speed = 5;
+            }
+        }
+    }
+    
+    public void onGround(){}
+    
+    public void jumpDie(){}
+    
+    public String collide(String dir, ArrayList <Block> block){
         
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        return this.collide;
     }
-    
-    /**
-     * when press the keyboard
-     */
-    public void keyPressed (KeyEvent argO) {
-        
-    }
-    
-    /**
-     * when release the keyboard
-     */
-    public void keyReleased(KeyEvent argO) {
-    }
-    
-    /**
-     * when input sth the keyboard
-     */
-    public void keyTyped(KeyEvent argO) {
-    }
-    
     
 }

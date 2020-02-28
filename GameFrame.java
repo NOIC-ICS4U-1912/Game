4dfb618ba5079d47fb67fb335db535ceb7f81c4b
Dave Zhao <36692409+Dave1007@users.noreply.github.com>
2020-02-28 15:22:28 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -4,67 +4,49 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 
-/**
- * Write a description of class GameFrame here.
- *
- * Leslie
- * 2.24
- */
-public class GameFrame extends JFrame implements KeyListener {
-    private boolean isStart = false;
-    MrX mrX = new MrX();
-    public static void main (String [] args) {
-        new GameFrame();
-    }
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Component;
+
+
+public class GameFrame extends JFrame
+{
+    private Character mrX;
+    private KeyListener levelOne;
+    private ArrayList<Block> blockOne = new ArrayList<Block>();
     
-    public GameFrame() {
+    
+   
+    public void mapConstructuer() { 
         this.setTitle("The Hardest Game in This Planet! ");
         this.setSize(1920,1080);
-                
-        this.addKeyListener(this);
+        
+        
+        for (int i = 0; i < blockOne.size();i++){
+            blockOne.get(i).setLocation​(blockOne.get(i).getPosX(),blockOne.get(i).getPosY());
+            blockOne.get(i).setSize (blockOne.get(i).getLength(),blockOne.get(i).getWidth());
+        }
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+    
+    public void addBlock(Block newBlock){
         
-        Image.init();
+        this.blockOne.add(newBlock);
+    }
+    
+    public void main (){
         
-        //this.MrX = new MrX();
-        BufferedImage image = new BufferedImage(1920,1090,BufferedImage.TYPE_3BYTE_BGR);
-        g.drawImage(image,0,0, this);
-    }
-    
-    public void paint (Graphics g) {
-        BufferedImage image = new BufferedImage();
-    }
-    
-    /**
-     * when press the keyboard
-     */
-    public void keyPressed (KeyEvent argO) {
-        if (isStart) {
-            if (key.getKeyCode() == 39) {
-                this.mrX.rightMove();
-            }
-            if (key.getKeyCode() == 37) {
-                this.mrX.leftMove();
-            }
-            if (key.getKeyCode() == 32) {
-                this.mrX.jump();
-            }
-        }
-    }
-    
-    /**
-     * when release the keyboard
-     */
-    public void keyReleased(KeyEvent argO) {
+        GameFrame theGame = new GameFrame();
+        this.mrX = new Character(new Position(1,1));
+        this.KeyListener =  new KeyListener(this);
         
-    }
-    
-    /**
-     * when input sth the keyboard
-     */
-    public void keyTyped(KeyEvent argO) {
     }
     
     
